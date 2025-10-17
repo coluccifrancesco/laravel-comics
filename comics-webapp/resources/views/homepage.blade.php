4395182212dccf8087307e5a5ddcecc612d78916
coluccifrancesco <coluccif2002@gmail.com>
2025-10-17 16:39:58 +0200

    Add initial structure for comics web application with components and layout

--- comics-webapp/resources/views/homepage.blade.php
+++ comics-webapp/resources/views/homepage.blade.php
@@ -1,13 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/sass/app.css', "resources/js/app.js"])
-    <title>Laravel DC Comics</title>
-</head>
-<body>
+@extends("layouts.master")
 
-    
-</body>
-</html>
+@section("title")
+    Laravel DC Comics
+@endsection
+
+@section("content")
+    <h1>Homepage</h1>
+
+    <p>Ecco i magazines</p>
+
+    {{-- Componente --}}
+    <x-comics-list>
+        {{-- Per inserire nel componente ciò che sta tra i due tag: {{ $slot }} oppure {{ $nome-slot }} nel componente --}}
+        
+        {{-- 
+            Invece nella view della pagina, per inserire più elementi da passare, utilizziamo per esempio:
+            <x-slot:title>
+                Titolo
+            </x-slot:title>
+
+            <x-slot:subtitle>
+                Sottotitolo
+            </x-slot:subtitle>
+        --}}
+    </x-comics-list>
+@endsection
---
+++ comics-webapp/resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+<header>
+
+    <div class="container-fluid px-3 py-4">
+        <nav class="d-flex justify-content-between align-items-center">
+
+            <img src="" alt="logo">
+
+            <ul class="list-unstyled d-flex justify-content-center align-items-center gap-2 mb-0">
+                <li><a>CHARACTERS</a></li>
+                <li><a>COMICS</a></li>
+                <li><a>MOVIES</a></li>
+                <li><a>TV</a></li>
+                <li><a>GAMES</a></li>
+                <li><a>COLLECTIBLES</a></li>
+                <li><a>VIDEOS</a></li>
+                <li><a>FANS</a></li>
+                <li><a>NEWS</a></li>
+                <li><a>SHOP</a></li>
+            </ul>
+        </nav>
+    </div>
+
+</header>
